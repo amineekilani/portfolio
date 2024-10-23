74376550502cd78e0251536e44560b8421b3c7ba
Amine Kilani <aminekilani901@gmail.com>
2024-10-23 23:18:09 +0100

    Added the date to the time

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function Time()
 {
-    var s=new Date().getSeconds();
-    var m=new Date().getMinutes();
+    today=new Date();
+    var y=new Date().getFullYear();
+    var mo=new Date().getMonth()+1;
+    var d=today.getDate();
     var h=new Date().getHours();
+    var mi=new Date().getMinutes();
+    var s=new Date().getSeconds();
     var suffixe=(h>=12)?"PM":"AM";
+    mo=(mo<10)?"0"+mo:mo;
+    d=(d<10)?"0"+d:d;
     h=(h>=12)?h-12:h;
     h=(h<10)?"0"+h:h;
-    m=(m<10)?"0"+m:m;
+    mi=(mi<10)?"0"+mi:mi;
     s=(s<10)?"0"+s:s;
     h=(h==0)?12:h;
-    document.getElementById("time").innerHTML="<b>"+h+":"+m+":"+s+" "+suffixe+"</b>";
+    document.getElementById("date_and_time").innerHTML="<b>"+d+"/"+mo+"/"+y+"<br>"+h+":"+mi+":"+s+" "+suffixe+"</b>";
 }
 setInterval(Time,1000)
 document.getElementById("year").innerHTML=new Date().getFullYear();
